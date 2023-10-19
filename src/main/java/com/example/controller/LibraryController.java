08e26541492ccea6f5215f8d40e47e109d3581dd
Atsushi Sakaguchi <ats.pantomime612@gmail.com>
2023-10-19 14:00:13 +0900

    Chapter4のLesson4の課題3の処理2で行き詰りました~

--- src/main/java/com/example/controller/LibraryController.java
+++ src/main/java/com/example/controller/LibraryController.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.entity.Library;
 import com.example.service.LibraryService;
+import com.example.service.LoginUser;
 
 @Controller
 @RequestMapping("library")
@@ -31,10 +33,18 @@ public class LibraryController {
     }
     
     
-    @GetMapping("/borrow/{id}")
+    @GetMapping("/borrow")
     public String borrowingForm(@RequestParam("id") Integer id, Model model) {
     	Library library = this.libraryService.findById(id);
     	model.addAttribute("library",library);
     	return "library/borrowingForm";
     }
+    
+    @PostMapping("/borrow/{id}")
+    public String borrow(@RequestParam("id") Integer id, @RequestParam("return_due_date") String returnDueDate, @AuthenticationPrincipal LoginUser loginUser) {
+//    Library library = this.libraryService.findById(id);
+    this.libraryService.update(id, itemForm);
+    
+    }
+    
 }
--- src/main/java/com/example/service/LibraryService.java
+++ src/main/java/com/example/service/LibraryService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.entity.Library;
+import com.example.form.LibraryForm;
 import com.example.repository.LibraryRepository;
 
 @Service
@@ -27,6 +28,13 @@ public class LibraryService {
 		Optional<Library> optionalLibrary = this.libraryRepository.findById(id); // Optional型→nullではない場合（値がある場合に実行される（if的な））
 		Library library = optionalLibrary.get(); // Optionl型に値が入っている場合、その値を返す
 		return library; // つまり 選択されたidの情報？を返す
+	}
+	
+	
+	public Library update(Integer id ,LibraryForm libraryForm) {
+		Library library = this.findById(id);
+		library.setUser_id(libraryForm.getUser_id());
+		
 	}
 
 }
